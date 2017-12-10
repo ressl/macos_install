f3647f01b2a2edcf74d06f39fa9cc9fb00e3a10f
Robert Ressl <r.ressl@safematix.com>
2017-12-11 00:34:36 +0100

    Add macOS config section and some db entrys

--- all.sql
+++ all.sql
@@ -5,10 +5,31 @@ CREATE TABLE IF NOT EXISTS software (
   method TEXT VARCHAR(5) NOT NULL,
   info TEXT VARCHAR(255)
 );
+CREATE TABLE IF NOT EXISTS config (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  method TEXT VARCHAR(20) NOT NULL,
+  category TEXT VARCHAR(255) NOT NULL,
+  domain TEXT VARCHAR(255) NOT NULL,
+  key TEXT VARCHAR(255) NOT NULL,
+  keytyp TEXT VARCHAR(10) NOT NULL,
+  keyvalue TEXT VARCHAR(25) NOT NULL,
+  info TEXT VARCHAR(255)
+);
+INSERT INTO config(method, category, domain, key, keytyp, keyvalue, info) VALUES ('defaults', 'window', 'NSGlobalDomain', 'NSAutomaticWindowAnimationsEnabled', 'bool', 'false', 'Disable animations when opening and closing windows.');
+INSERT INTO config(method, category, domain, key, keytyp, keyvalue, info) VALUES ('defaults', 'window', 'NSGlobalDomain', 'QLPanelAnimationDuration', 'float', '0', 'Disable animations when opening a Quick Look window.');
+INSERT INTO config(method, category, domain, key, keytyp, keyvalue, info) VALUES ('defaults', 'window', 'NSGlobalDomain', 'NSWindowResizeTime', 'float', '0.001', 'Accelerated playback when adjusting the window size (Cocoa applications).');
+INSERT INTO config(method, category, domain, key, keytyp, keyvalue, info) VALUES ('defaults', 'finder', 'com.apple.finder', 'DisableAllAnimations', 'bool', 'true', 'Disable animation when opening the Info window in Finder (cmd + i).');
+INSERT INTO config(method, category, domain, key, keytyp, keyvalue, info) VALUES ('defaults', 'dock', 'com.apple.dock', 'launchanim', 'bool', 'false', 'Disable animations when you open an application from the Dock.');
+INSERT INTO config(method, category, domain, key, keytyp, keyvalue, info) VALUES ('defaults', 'dock', 'com.apple.dock', 'expose-animation-duration', 'float', '0.1', 'Make all animations faster that are used by Mission Control.');
+INSERT INTO config(method, category, domain, key, keytyp, keyvalue, info) VALUES ('defaults', 'dock', 'com.apple.dock', 'autohide-delay', 'float', '0', 'Disable the delay when you hide the Dock');
 INSERT INTO software(name, category, method) VALUES ('caskroom/cask', 'tap', 'tap');
 INSERT INTO software(name, category, method) VALUES ('caskroom/drivers', 'tap', 'tap');
 INSERT INTO software(name, category, method) VALUES ('caskroom/versions', 'tap', 'tap');
 INSERT INTO software(name, category, method) VALUES ('caskroom/fonts', 'tap', 'tap');
+INSERT INTO software(name, category, method) VALUES ('font-source-code-pro', 'font', 'cask');
+INSERT INTO software(name, category, method) VALUES ('font-dejavu-sans', 'font', 'cask');
+INSERT INTO software(name, category, method) VALUES ('font-monoid', 'font', 'cask');
+INSERT INTO software(name, category, method) VALUES ('font-raleway', 'font', 'cask');
 INSERT INTO software(name, category, method) VALUES ('adobe-creative-cloud', 'multimedia', 'cask');
 INSERT INTO software(name, category, method) VALUES ('bartender', 'multimedia', 'cask');
 INSERT INTO software(name, category, method) VALUES ('bash', 'core', 'core');
